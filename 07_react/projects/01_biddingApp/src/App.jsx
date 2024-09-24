eaf714224feb5fa502c95958e83824f280f11939
Nehal Ahmed <m.nehalq125@gmail.com>
2024-09-24 16:32:04 +0500

    initialize project file or pages

--- 07_react/projects/01_biddingApp/src/App.jsx
+++ 07_react/projects/01_biddingApp/src/App.jsx
@@ -1,9 +1,21 @@
-import { useState } from 'react'
+import { Outlet, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import SignUp from './pages/Auth/SignUp'
+import LogIn from './pages/Auth/LogIn'
+import Cart from './pages/Home/Cart'
 
 function App() {
   return (
-    <>
-    </>
+    <Routes>
+      <Route path='/auth' element={<Outlet />}>
+        <Route index element={<LogIn />} />
+        <Route path='signup' element={<SignUp />} />
+      </Route>
+      <Route path={'/'} element={<Outlet />}>
+        <Route index element={<Home />} />
+        <Route path='cart' element={<Cart />} />
+      </Route>
+    </Routes>
   )
 }
 
