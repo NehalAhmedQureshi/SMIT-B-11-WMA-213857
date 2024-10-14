6db1fe9bbf7cd0ab62c9773bbd83b0dae4b2182e
Nehal Ahmed <m.nehalq125@gmail.com>
2024-10-14 23:32:31 +0500

    add revision

---
+++ 04_Java-Script/Revision/01-alertPrompt.js
@@ -0,0 +1,37 @@
+
+//*  var , let , const 
+
+//* scopes
+// todo 1-block scope { this is a block scope }
+//!  2 global scopes
+
+const username = 'abc'
+// {
+// }
+// console.log("🚀 ~ name:", username)
+
+// ! alert consoles
+// alert('My name is ', username)
+// console.table("My name is " , username, "my father name is " , username)
+
+let num = 34
+let string = 'in'
+let bool = true
+let array = [34 ,34]
+let obj = {
+     name :'nehal'     
+}
+let char = Symbol('a')
+Symbol('a')
+
+// * if else or else if
+// if (Symbol('a') == Symbol('a')){
+//      console.log('if wali condition m hu')
+// }else if(num == 34) {
+//      console.log('if else wali condition m hu')
+// }else if(bool) {
+//      console.log('if bool wali condition m hu')
+// }
+// else {
+//      console.log('majbooran chala hu')
+// }
